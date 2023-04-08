6e86b5bfc21a4c76cf0f2794a1532e05ea52edc0
zhijazi <zein@hijazi.dev>
2023-04-08 15:24:54 -0400

    initial dotfiles

---
+++ .config/nvim/lua/plugins/text.lua
@@ -0,0 +1,11 @@
+return {
+  { "tpope/vim-sleuth" },
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({})
+    end
+  }
+}
